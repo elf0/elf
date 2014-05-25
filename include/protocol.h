bae8a1996b7f2468a97da623be787b4edad9db1c
elf <elf198012@gmail.com>
2014-05-26 00:51:18 +0800

    Modify file 'protocol.h'

--- include/protocol.h
+++ include/protocol.h
@@ -1,16 +1,25 @@
 #pragma once
 
+//packet: | size(2 bytes) | type(2 bytes) | verify code(3 bytes) | data(size - 5 bytes) |
+ 
+#define PROTOCOL_VERSION 0
+
+#define PACKET_BUFFER_SIZE 65536
+
+//4 bytes
+#define PACKET_HEADER_FIELDS \
+  U16 nSize; \
+  U16 nType
+
 typedef struct{
-  U16 nSize; //packet size, not include 'nSize' field
-  U16 nType; //enum PacketType
-  U32 nVerifyCode; //packet verify code, not include 'nVerifyCode' field
-  Byte szData[65528];
+  U16 nSize; //data size
+  Byte szData[PACKET_BUFFER_SIZE - sizeof(nSize)];
 }Packet;
 
 typedef enum{
     ptHello, //Handshake. Protocol version, Crypto type.
     ptBye, //Can close connection or session now
-    ptDescription, //descript node
+    ptNode, //descript node
     ptProxy, //Pass data to target node or site.
     ptCode, //Help me execute the code/function, and return result to me.
     ptLocation, //This node location(GPS). Usually for private session.
@@ -32,3 +41,24 @@ typedef enum{
     ptAudioMeta,
     ptAudioData
 }PacketType;
+
+typedef enum{
+ ctCustom, ctAES, ctRSA
+}CryptoType;
+
+//12 bytes
+typedef struct{
+  PACKET_HEADER_FIELDS;
+  U32 nVerifyCode: 24; //accumulate packet bytes, include header and data, but not include 'nVerifyCode' field
+  U32 nCrypto: 8;
+  U32 nVersion; //protocol version
+}Packet_Hello;
+
+
+typedef struct{
+  PACKET_HEADER_FIELDS;
+  U32 bSharingFile: 1;
+  U32 bSharingDisk: 1;
+  U32 bSharingProcessor: 1;
+}Packet_Node;
+
