0ff529fee3770e93b5516b6e272a6c2b2b6f41ef
elf <elf198012@gmail.com>
2014-05-25 23:53:20 +0800

    Modify file 'protocol.h'

--- include/protocol.h
+++ include/protocol.h
@@ -1,20 +1,20 @@
 #pragma once
 
 typedef struct{
-  U16 nSize;//packet size, not include nSize field
-  U16 nType;
-  U32 nVerifyCode;
+  U16 nSize; //packet size, not include 'nSize' field
+  U16 nType; //enum PacketType
+  U32 nVerifyCode; //packet verify code, not include 'nVerifyCode' field
   Byte szData[65528];
 }Packet;
 
 typedef enum{
     ptHello, //Handshake. Protocol version, Crypto type.
-    ptBye, //Remote can close connection or session now
-    ptDescription, //This node'description.
-    ptProxy, //Pass my data to target node or site.
+    ptBye, //Can close connection or session now
+    ptDescription, //descript node
+    ptProxy, //Pass data to target node or site.
     ptCode, //Help me execute the code/function, and return result to me.
     ptLocation, //This node location(GPS). Usually for private session.
-    ptSOS, //Help me! I'm in danger. For prefined nodes(family, friends, polices...), or every one who can help me.
+    ptSOS, //Help me! I'm in danger. Send to prefined nodes(family, friends, polices...), or every one who can help me.
     ptMessage, //Text chat
     ptDicMessage, //Dictionary-based chat
 //File
