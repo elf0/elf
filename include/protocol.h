30cd6a999c731b6fb8753820a6f69d17fca0ea65
elf <elf198012@gmail.com>
2015-01-05 14:35:23 +0800

    Modify protocol.h

--- include/protocol.h
+++ include/protocol.h
@@ -15,29 +15,29 @@ Process order:
 #define PACKET_BUFFER_SIZE 65536
 
 typedef enum{
-    ptHello, //Handshake. Protocol version, Crypto type.
-    ptBye, //Can close connection or session now
-    ptNode, //descript node
-    ptProxy, //Pass data to target node or site.
-    ptCode, //Help me execute the code/function, and return result to me.
-    ptLocation, //This node location(GPS). Usually for private session.
-    ptSOS, //Help me! I'm in danger. Send to prefined nodes(family, friends, polices...), or every one who can help me.
-    ptMessage, //Text chat
-    ptDicMessage, //Dictionary-based chat
+    pktHello, //Handshake. Protocol version, Crypto type.
+    pktBye, //Can close connection or session now
+    pktNode, //descript node
+    pktProxy, //Pass data to target node or site.
+    pktCode, //Help me execute the code/function, and return result to me.
+    pktLocation, //This node location(GPS). Usually for private session.
+    pktSOS, //Help me! I'm in danger. Send to prefined nodes(family, friends, polices...), or every one who can help me.
+    pktMessage, //Text chat
+    pktDicMessage, //Dictionary-based chat
 //File
-    ptGetFile, //Download file
-    ptStoreFile, //Store my file in net. File must split into many blocks, and store at many nodes, many countries. Some nodes/countries broke won't lost data.
-    ptFindFile, //Find file in net
-    ptFileMeta, //file name, size, last modified time...
-    ptFileData,
+    pktGetFile, //Download file
+    pktStoreFile, //Store my file in net. File must split into many blocks, and store at many nodes, many countries. Some nodes/countries broke won't lost data.
+    pktFindFile, //Find file in net
+    pktFileMeta, //file name, size, last modified time...
+    pktFileData,
 //Directory for file system
-    ptDirectoryMeta, //directory name, size, last modified time...
-    ptDirectoryData,
+    pktDirectoryMeta, //directory name, size, last modified time...
+    pktDirectoryData,
 //Real-time Stream
-    ptVideoMeta,
-    ptVideoData,
-    ptAudioMeta,
-    ptAudioData
+    pktVideoMeta,
+    pktVideoData,
+    pktAudioMeta,
+    pktAudioData
 }PacketType;
 
 typedef enum{
