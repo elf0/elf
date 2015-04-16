b8becfc27f9a10e695009c5fdcaaa431a6a21961
elf <elf198012@gmail.com>
2015-04-16 10:59:40 +0800

    Modify Elf.h

--- include/Elf.h
+++ include/Elf.h
@@ -1,3 +1,6 @@
+#ifndef ELF_H
+#define ELF_H
+
 //License: Public Domain
 //Author: elf
 //EMail: elf198012@gmail.com
@@ -11,4 +14,10 @@ void Elf_Disconnect(Elf *pElf, Net *pNet);
 void Elf_What(Elf *pElf, const Char *pKey, U16 nBytes);
 void Elf_Where(Elf *pElf, const Char *pKey, U16 nBytes);
 void Elf_Locate(Elf *pElf, const Location *pLocation);
+void Elf_PlayMusic(Elf *pElf, const Char *pName, U16 nBytes);
+void Elf_PlayMovie(Elf *pElf, const Char *pName, U16 nBytes);
+void Elf_Weather(Elf *pElf);
+void Elf_Traffic(Elf *pElf);
+
+#endif //ELF_H
 
