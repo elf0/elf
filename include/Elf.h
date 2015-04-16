4332b69a7e6b3d8fe347f98a7fe2fa9d94bf3e6e
elf <elf198012@gmail.com>
2015-04-16 13:16:54 +0800

    Modify Elf.h

--- include/Elf.h
+++ include/Elf.h
@@ -16,8 +16,6 @@ void Elf_Disconnect(Elf *pElf, Net *pNet);
 void Elf_What(Elf *pElf, const Char *pKey, U16 nBytes);
 void Elf_Weather(Elf *pElf);
 void Elf_Traffic(Elf *pElf);
-void Elf_Music_Play(Elf *pElf, const Char *pName, U16 nBytes);
-void Elf_Movie_Play(Elf *pElf, const Char *pName, U16 nBytes);
 void Elf_Game_Play(Elf *pElf, const Char *pName, U16 nBytes);
 
 //Picture
@@ -32,10 +30,12 @@ void Elf_Picture_Nature_Search(Elf *pElf);
 //Video
 void Elf_Video_Upload(Elf *pElf, const Char *pPathName, U16 nBytes);
 void Elf_Video_XXX_Search(Elf *pElf);
-void Elf_Video_Movie_Search(Elf *pElf);
-void Elf_Video_Cartoon_Search(Elf *pElf);
 void Elf_Video_Funny_Search(Elf *pElf);
 void Elf_Video_Nature_Search(Elf *pElf);
+void Elf_Movie_Search(Elf *pElf);
+void Elf_Movie_Play(Elf *pElf, const Char *pName, U16 nBytes);
+void Elf_Cartoon_Search(Elf *pElf);
+void Elf_Cartoon_Play(Elf *pElf, const Char *pName, U16 nBytes);
 
 //3D model
 void Elf_3DModel_Upload(Elf *pElf, const Char *pPathName, U16 nBytes);
@@ -47,10 +47,12 @@ void Elf_3DModel_Building_Search(Elf *pElf);
 
 //Audio
 void Elf_Audio_Upload(Elf *pElf, const Char *pPathName, U16 nBytes);
-void Elf_Audio_Music_Search(Elf *pElf);
-void Elf_Audio_Song_Search(Elf *pElf);
 void Elf_Audio_XXX_Search(Elf *pElf);
 void Elf_Audio_Story_Search(Elf *pElf);
+void Elf_Music_Search(Elf *pElf);
+void Elf_Music_Play(Elf *pElf, const Char *pName, U16 nBytes);
+void Elf_Song_Search(Elf *pElf);
+void Elf_Song_Play(Elf *pElf, const Char *pName, U16 nBytes);
 
 //Map
 void Elf_Map_Download(Elf *pElf, RectangleF32 *pRect);
