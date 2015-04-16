e0bf55e6b1e87d80269a7ab61871020edbc64aec
elf <elf198012@gmail.com>
2015-04-16 13:07:33 +0800

    Modify Elf.h

--- include/Elf.h
+++ include/Elf.h
@@ -16,38 +16,41 @@ void Elf_Disconnect(Elf *pElf, Net *pNet);
 void Elf_What(Elf *pElf, const Char *pKey, U16 nBytes);
 void Elf_Weather(Elf *pElf);
 void Elf_Traffic(Elf *pElf);
-void Elf_PlayMusic(Elf *pElf, const Char *pName, U16 nBytes);
-void Elf_PlayMovie(Elf *pElf, const Char *pName, U16 nBytes);
-void Elf_PlayGame(Elf *pElf, const Char *pName, U16 nBytes);
+void Elf_Music_Play(Elf *pElf, const Char *pName, U16 nBytes);
+void Elf_Movie_Play(Elf *pElf, const Char *pName, U16 nBytes);
+void Elf_Game_Play(Elf *pElf, const Char *pName, U16 nBytes);
 
 //Picture
 void Elf_Picture_Upload(Elf *pElf, const Char *pPathName, U16 nBytes);
-void Elf_Picture_SearchFemale(Elf *pElf);
-void Elf_Picture_SearchMale(Elf *pElf);
-void Elf_Picture_SearchPlant(Elf *pElf);
-void Elf_Picture_SearchAnimal(Elf *pElf);
-void Elf_Picture_SearchBuilding(Elf *pElf);
+void Elf_Picture_Female_Search(Elf *pElf);
+void Elf_Picture_Male_Search(Elf *pElf);
+void Elf_Picture_Plant_Search(Elf *pElf);
+void Elf_Picture_Animal_Search(Elf *pElf);
+void Elf_Picture_Building_Search(Elf *pElf);
+void Elf_Picture_Nature_Search(Elf *pElf);
 
 //Video
 void Elf_Video_Upload(Elf *pElf, const Char *pPathName, U16 nBytes);
-void Elf_Video_SearchXXX(Elf *pElf);
-void Elf_Video_SearchMovie(Elf *pElf);
-void Elf_Video_SearchCartoon(Elf *pElf);
-void Elf_Video_SearchFunny(Elf *pElf);
+void Elf_Video_XXX_Search(Elf *pElf);
+void Elf_Video_Movie_Search(Elf *pElf);
+void Elf_Video_Cartoon_Search(Elf *pElf);
+void Elf_Video_Funny_Search(Elf *pElf);
+void Elf_Video_Nature_Search(Elf *pElf);
 
 //3D model
 void Elf_3DModel_Upload(Elf *pElf, const Char *pPathName, U16 nBytes);
-void Elf_3DModel_SearchFemale(Elf *pElf);
-void Elf_3DModel_SearchMale(Elf *pElf);
-void Elf_3DModel_SearchPlant(Elf *pElf);
-void Elf_3DModel_SearchAnimal(Elf *pElf);
-void Elf_3DModel_SearchBuilding(Elf *pElf);
+void Elf_3DModel_Female_Search(Elf *pElf);
+void Elf_3DModel_Male_Search(Elf *pElf);
+void Elf_3DModel_Plant_Search(Elf *pElf);
+void Elf_3DModel_Animal_Search(Elf *pElf);
+void Elf_3DModel_Building_Search(Elf *pElf);
 
 //Map
-void Elf_DownloadMap(Elf *pElf, RectangleF32 *pRect);
-void Elf_DownloadMapOfCity(Elf *pElf, const Char *pName, U16 nBytes);
-void Elf_DownloadMapOfProvince(Elf *pElf, const Char *pName, U16 nBytes);
-void Elf_DownloadMapOfCountry(Elf *pElf, const Char *pName, U16 nBytes);
+void Elf_Map_Download(Elf *pElf, RectangleF32 *pRect);
+void Elf_Map_City_Download(Elf *pElf, const Char *pName, U16 nBytes);
+void Elf_Map_Province_Download(Elf *pElf, const Char *pName, U16 nBytes);
+void Elf_Map_Country_Download(Elf *pElf, const Char *pName, U16 nBytes);
+void Elf_Map_Earth_Download(Elf *pElf);
 
 //Location
 void Elf_Where(Elf *pElf, const Char *pKey, U16 nBytes);
